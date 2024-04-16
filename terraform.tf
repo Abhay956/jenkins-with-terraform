a54c2e78294d81c82e449361db5c3c5158723f06
Abhay956 <132220412+Abhay956@users.noreply.github.com>
2024-04-17 03:38:37 +0530

    Update terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "myinstance1" {
 ami = "ami-0b8b44ec9a8f90422"
 instance_type = "t2.micro"
-key_name = "TF-key"
+key_name = "ohio"
 associate_public_ip_address = true
 tags = {
         Name = "Webserver1"

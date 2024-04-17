705c71a374adf0f80deb56beb60cd4b27a7733b4
Abhay956 <132220412+Abhay956@users.noreply.github.com>
2024-04-17 13:48:07 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 sudo apt update && apt install apache2 -y
-sudo echo "<html><head><style>body{background-color:black;color:lightblue;}</style></head><body><h1>hello from Abhay Pande</h1> </body></html>" > /var/www/html/index.html
 sudo systemctl start apache2
 sudo systemctl enable apache2
+sudo echo "<html><head><style>body{background-color:black;color:lightblue;}</style></head><body><h1>hello from Abhay Pande</h1> </body></html>" > /var/www/html/index.html
 

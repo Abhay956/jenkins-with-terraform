f3472487e2fe77f4005b06d2e1e713785bf221f5
Abhay956 <132220412+Abhay956@users.noreply.github.com>
2024-04-17 13:40:34 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
-sudo yum update && yum install httpd -y
+sudo apt update && apt install apache2 -y
 sudo echo "<html><head><style>body{background-color:black;color:lightblue;}</style></head><body><h1>hello from Abhay Pande</h1> </body></html>" > /var/www/html/index.html
-sudo systemctl start httpd
-sudo systemctl enable httpd 
+sudo systemctl start apache2
+sudo systemctl enable apache2
 

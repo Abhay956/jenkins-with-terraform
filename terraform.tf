c2e6a90fb90b4e135fca98d4ab2fed075a5274f1
Abhay956 <132220412+Abhay956@users.noreply.github.com>
2024-04-17 13:33:46 +0530

    Update terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -75,7 +75,7 @@ resource "aws_security_group" "allow_ssh_http" {
 
 ## VM with user data for webserver
 resource "aws_instance" "myinstance1" {
-  ami = "ami-079db87dc4c10ac91"
+  ami = "ami-0b8b44ec9a8f90422"
   instance_type = "t2.micro"
   key_name = "ohio"
   associate_public_ip_address = true

61ae330d484e79cc5f1757afb7bbb08b196c1380
Abhay956 <132220412+Abhay956@users.noreply.github.com>
2024-04-17 13:58:43 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -2,5 +2,5 @@
 sudo apt update && apt install apache2 -y
 sudo systemctl start apache2
 sudo systemctl enable apache2
-sudo echo "<html><head><style>body{background-color:black;color:lightblue;}</style></head><body><h1>Hello From Unnati</h1> </body></html>" > /var/www/html/index.html
+sudo echo "<html><head><style>body{background-color:black;color:lightblue;}</style></head><body><h1>Apache has been successfully installed</h1> </body></html>" > /var/www/html/index.html
 
